ad5f3587d4ab2659a7e4f56c6cfc220cb64e8ac0
Ryan Sun <sunryan20@gmail.com>
2025-06-11 23:49:43 -0700

    unrollback mash changes

--- README.md
+++ README.md
@@ -2,5 +2,7 @@ Discord bot to quiz yourself on NFL player images\
 Usage:\
 rat.quiz: Random NFL Player\
 rat.face: Random NFL Player (Face Only)\
+rat.mash: NFL Player Morph (2 Faces)\ 
 rat.stats: quiz leaderboard\
 rat.facestats: face leaderboard\
+rat.mashstats: face morph leaderboard\
--- startdb.py
+++ startdb.py
@@ -5,4 +5,4 @@ cur = conn.cursor()
 
 cur.execute("CREATE TABLE IF NOT EXISTS users(name PRIMARY KEY, plays, correct, currstreak, maxstreak)")
 cur.execute("CREATE TABLE IF NOT EXISTS faceusers(name PRIMARY KEY, plays, correct, currstreak, maxstreak)")
-# cur.execute("CREATE TABLE IF NOT EXISTS mashusers(name PRIMARY KEY, plays, correct, currstreak, maxstreak)")
+cur.execute("CREATE TABLE IF NOT EXISTS mashusers(name PRIMARY KEY, plays, correct, currstreak, maxstreak)")
